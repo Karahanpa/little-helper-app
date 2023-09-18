18a9694f63bcc06e466afae479a52ce3931064b2
Karahanpa <karaaslanka@gmail.com>
2023-09-19 00:05:54 +0300

    https

--- src/Weather/WeatherConnect.js
+++ src/Weather/WeatherConnect.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WeatherConnect = (city) => {
     const apiKey = "b6a530b72ec64d55906103932231509";
-    return fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`)
+    return fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`)
     .then((response) => {
         if (!response.ok) {
             throw new Error("network response was not ok");
